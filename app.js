971de3bdc6cd4353a35ea19b2d2d8a11cb1132ef
Steven Roberts <contact@stevenroberts.info>
2018-03-20 15:22:58 +0000

    added the request HTTP call into the app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const request = require('request');
 
+const keys = require("./API-KEYS");
+
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=B901HD'
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=B901HD&key=${keys.google}`,
+    json: true
 
 },(error, response, body) => {
     console.log(body);
